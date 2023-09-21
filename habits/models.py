570ced0e9b04ef315331c92753119512f81c28b7
Dmitrii <dima12sik@yandex.ru>
2023-09-21 15:07:13 +0300

    added HabitLog and PleasantHabit models

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+
 # Create your models here.
+class PleasantHabit(models.Model):
+    class Kinds(models.TextChoices):
+        DAILY = ('Ежедневно', 'Ежедневно')
+        ONE = ('Один раз в неделю', 'Один раз в неделю')
+        TWICE = ('Два раз в неделю', 'Два раз в неделю')
+        THREE = ('Три раз в неделю', 'Три раз в неделю')
+        FOUR = ('Четыре раз в неделю', 'Четыре раз в неделю')
+        FIVE = ('Пять раз в неделю', 'Пять раз в неделю')
+        SIX = ('Шесть раз в неделю', 'Шесть раз в неделю')
+
+    habit_user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='владелец привычки')
+    place = models.CharField(max_length=150, verbose_name='место')
+    date_time = models.DateTimeField(verbose_name='время привычки')
+    action = models.CharField(max_length=150, verbose_name='действие')
+    interval = models.CharField(max_length=19, choices=Kinds.choices, default=Kinds.DAILY, verbose_name='периодичность')
+    execution_time = models.TimeField(verbose_name='время выполнения')
+    is_published = models.BooleanField(default=False, verbose_name='время выполнения')
+
+    def __str__(self):
+        return f'я буду {self.action} в {self.date_time} в {self.place}'
+
+    class Meta:
+        verbose_name = 'полезная привычка'
+        verbose_name_plural = 'полезные привычки'
+        ordering = ('habit_user',)
+
+
+class HabitLog(models.Model):
+    habit = models.ForeignKey('habits.PleasantHabit', on_delete=models.CASCADE, verbose_name='привычка')
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='владелец')
+    status = models.BooleanField(default=False, verbose_name='статус')
+    last_try = models.DateTimeField(auto_now_add=True, verbose_name='попытка')
+
+    class Meta:
+        verbose_name = 'Информация'
+        verbose_name_plural = 'Информации'
