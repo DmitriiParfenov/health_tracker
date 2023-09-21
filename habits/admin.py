e73269b5cbc7e6ddb90b20dd86375311740d7446
Dmitrii <dima12sik@yandex.ru>
2023-09-21 17:54:29 +0300

    added id field

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from habits.models import PleasantHabit
+
+
 # Register your models here.
+@admin.register(PleasantHabit)
+class PleasantHabitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'place', 'date_time', 'action', 'interval', 'execution_time', 'is_published', 'habit_user')
+    list_display_links = ('id', )
+    search_fields = ('date_time', 'habit_user')
+    list_filter = ('date_time', 'habit_user')
