e4f2e7e4950fce26f72f5ea07ef63070c5c291f4
Dmitrii <dima12sik@yandex.ru>
2023-09-21 17:55:03 +0300

    added path to the habits.urls

--- config/urls.py
+++ config/urls.py
@@ -43,5 +43,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('user/', include('users.urls', namespace='user'))
+    path('user/', include('users.urls', namespace='user')),
+    path('habits/', include('habits.urls', namespace='habits'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
