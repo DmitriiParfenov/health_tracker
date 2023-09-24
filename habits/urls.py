a7b0a92391b13c021665224a7ee3bc61d7d7c1a0
Dmitrii <dima12sik@yandex.ru>
2023-09-24 19:03:37 +0300

    added paths for all views.

--- habits/urls.py
+++ habits/urls.py
@@ -1,15 +1,24 @@
 from django.urls import path
 
 from habits.apps import HabitsConfig
-from habits.views import PleasantHabitListAPIView, PleasantHabitCreateAPIView, PleasantHabitRetrieveAPIView, \
+from habits.views import HabitListAPIView, HabitCreateAPIView, HabitRetrieveAPIView, HabitUpdateAPIView, \
+    HabitDeleteAPIView, PleasantHabitListAPIView, PleasantHabitCreateAPIView, PleasantHabitRetrieveAPIView, \
     PleasantHabitUpdateAPIView, PleasantHabitDeleteAPIView
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
-    path('pleasant_habits/', PleasantHabitListAPIView.as_view(), name='pleasant_list'),
-    path('pleasant_habits/create/', PleasantHabitCreateAPIView.as_view(), name='pleasant_create'),
-    path('pleasant_habits/<int:pk>/', PleasantHabitRetrieveAPIView.as_view(), name='pleasant_detail'),
-    path('pleasant_habits/update/<int:pk>/', PleasantHabitUpdateAPIView.as_view(), name='pleasant_update'),
-    path('pleasant_habits/delete/<int:pk>/', PleasantHabitDeleteAPIView.as_view(), name='pleasant_delete'),
+    # Привычки для формирования — Habit model.
+    path('', HabitListAPIView.as_view(), name='habit_list'),
+    path('create/', HabitCreateAPIView.as_view(), name='habit_create'),
+    path('<int:pk>/', HabitRetrieveAPIView.as_view(), name='habit_detail'),
+    path('update/<int:pk>/', HabitUpdateAPIView.as_view(), name='habit_update'),
+    path('delete/<int:pk>/', HabitDeleteAPIView.as_view(), name='habit_delete'),
+
+    # Приятные привычки — PleasantHabit model.
+    path('pleasant/', PleasantHabitListAPIView.as_view(), name='pleasant_habit_list'),
+    path('pleasant/create/', PleasantHabitCreateAPIView.as_view(), name='pleasant_habit_create'),
+    path('pleasant/<int:pk>/', PleasantHabitRetrieveAPIView.as_view(), name='pleasant_habit_detail'),
+    path('pleasant/update/<int:pk>/', PleasantHabitUpdateAPIView.as_view(), name='pleasant_habit_update'),
+    path('pleasant/delete/<int:pk>/', PleasantHabitDeleteAPIView.as_view(), name='pleasant_habit_delete'),
 ]
