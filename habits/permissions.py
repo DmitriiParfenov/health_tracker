e1d0990cd741c13acbcbe1b3ebca01f5de3f4282
Dmitrii <dima12sik@yandex.ru>
2023-09-24 19:02:02 +0300

    added IsAuthenticatedAndIsOwner - Each user has access only to his habits by CRUD mechanism. - The user can see the list of public habits without being able to edit or delete them

--- habits/permissions.py
+++ habits/permissions.py
@@ -2,6 +2,11 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAuthenticatedAndIsOwner(BasePermission):
+    """
+    Доступ разрешен только аутентифицированным пользователям. Детали записей могут смотреть только владельцы записей.
+    Пользователь может видеть список публичных привычек (поле <is_published> есть True) без возможности их
+    как-то редактировать или удалять.
+    """
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return True
