5301820983f92d42e053faa867f0631e07a0dccf
Dmitrii <dima12sik@yandex.ru>
2023-09-24 18:07:07 +0300

    updated docstring

--- users/permissions.py
+++ users/permissions.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import BasePermission
 class IsAuthenticatedOrIsOwner(BasePermission):
     """
     Доступ для безопасных методов разрешен только аутентифицированным пользователям. Расширенную информацию могут
-    смотреть, а также ее обновлять, только владельцы записи.
+    смотреть, а также ее обновлять, только владельцы записей.
     """
     def has_permission(self, request, view):
         if request.user.is_authenticated:
