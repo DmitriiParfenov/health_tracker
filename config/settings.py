5b76bb8f7ad31ca395e6efeefcc7dbdc540c1095
Dmitrii <dima12sik@yandex.ru>
2023-09-27 06:57:38 +0300

    updated ACCESS_TOKEN_LIFETIME

--- config/settings.py
+++ config/settings.py
@@ -143,7 +143,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=55),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
